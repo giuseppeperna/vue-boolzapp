fb6de2555c4510e885a06f01570362fae1a4728d
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-20 15:02:17 +0100

    BoolzApp
    Input text bar completed

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
-const usersAvatar = [];
+const usersAvatar = [
+  "img/avatar_1.jpg",
+  "img/avatar_2.jpg",
+  "img/avatar_3.jpg",
+  "img/avatar_4.jpg",
+  "img/avatar_5.jpg",
+  "img/avatar_6.jpg",
+  "img/avatar_7.jpg",
+  "img/avatar_8.jpg",
+];
 
 const boolzApp = new Vue({
   el:'#boolzApp',
   data: {
     mainUser: {
         name:"Nome Utente",
-        avatar:"",
+        avatar:"avatar_io.jpg",
       },
     contacts: [
       {
         name:"Nome Utente",
-        avatar:"",
+        avatar: "img/avatar_1.jpg",
         lastAccess:"",
       }
     ]
