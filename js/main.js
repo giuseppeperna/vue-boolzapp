6fd3f969633ddb488d08178fd881140d86295692
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-20 13:33:07 +0100

    BoolzApp
    Html layout

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const usersAvatar = [];
+
+const boolzApp = new Vue({
+  el:'#boolzApp',
+  data: {
+    mainUser: {
+        name:"Nome Utente",
+        avatar:"",
+      },
+    contacts: [
+      {
+        name:"Nome Utente",
+        avatar:"",
+        lastAccess:"",
+      }
+    ]
+  },
+  methods: {
+
+  }
+})
