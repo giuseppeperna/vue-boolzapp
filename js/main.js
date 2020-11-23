a64b7de9ea437c9a3811a2972ec9a860181d983b
Giuseppe Perna <giuseppeperna.cg@gmail.com>
2020-11-23 18:57:45 +0100

    BoolzApp
    New implemented features:
    1) Chat message drop-down menu.
    2) Delete a message.

--- js/main.js
+++ js/main.js
@@ -8,13 +8,14 @@ Milestone 2 =
 - I messaggi, oltre al testo e alla data, hanno un determinato stato, ricevuto o inviato, e in base a questo deve mostrarsi come in figura.
 
 Altre Features implementate:
-- Selezionare una chat tra quelle disponibili.
 - Filter sulla ricerca delle chat presenti.
 - Possibilità di inviare un messaggio in ogni chat e ricevere una risposta random in automatico.
 - Sostituzione dinamica dell'icona di invio messaggio solo quando c'è del testo nel campo input.
 - Toggle On/Off dell'icona e della descrizione delle notifiche desktop.
 - Trasformazione dinamica dell'ora di ultimo accesso alla chat e dell'ora dei messaggi inviati.
 - Nella lista delle chat disponibili viene mostrata l'anteprima dell'ultimo messaggio e l'orario di invio/ricezione.
+- Toggle sulla visibilità dei dropdown menus per ogni messaggio della chat.
+- Possibilità di cancellare un messaggio dalla chat.
 
 @author Giuseppe Perna <giuseppeperna.cg@gmail.com>
 */
@@ -54,21 +55,25 @@ const boolzApp = new Vue({
             text:"A che ore ci incontriamo?",
             userId: this.chatId,
             time:"09:26",
+            dropdownMenu: false,
           },
           {
             text:"Ti va bene alle 12:00 in piazza?",
             userId: 0,
             time:"09:50",
+            dropdownMenu: false,
           },
           {
             text:"Se faccio tardi, ti avviso.",
             userId: 0,
             time:"10:00",
+            dropdownMenu: false,
           },
           {
             text:"Perfetto! A dopo.",
             userId: this.chatId,
             time:"10:15",
+            dropdownMenu: false,
           },
         ],
         randomAnswers: [
@@ -89,16 +94,19 @@ const boolzApp = new Vue({
             text:"La cena di ieri è stata fantastica!",
             userId: 0,
             time:"15:18",
+            dropdownMenu: false,
           },
           {
             text:"Vogliamo parlare del vino? TOP!",
             userId: this.chatId,
             time:"15:26",
+            dropdownMenu: false,
           },
           {
             text:"Il vino era davvero ottimo, ma il dessert rimane insuperabile.",
             userId: 0,
             time:"15:32",
+            dropdownMenu: false,
           },
         ],
         randomAnswers: [
@@ -119,21 +127,25 @@ const boolzApp = new Vue({
             text:"Pronto per la partita?",
             userId: this.chatId,
             time:"19:03",
+            dropdownMenu: false,
           },
           {
             text:"Sono carico a mille!",
             userId: 0,
             time:"19:07",
+            dropdownMenu: false,
           },
           {
             text:"Dai che stasera vinciamo!",
             userId: this.chatId,
             time:"19:11",
+            dropdownMenu: false,
           },
           {
             text:"Me lo sento.",
             userId: this.chatId,
             time:"19:12",
+            dropdownMenu: false,
           },
         ],
         randomAnswers: [
@@ -154,21 +166,25 @@ const boolzApp = new Vue({
             text:"Ciao Luisa! Com'è andato l'esame?",
             userId: 0,
             time:"11:26",
+            dropdownMenu: false,
           },
           {
             text:"Ciao! Molto bene, grazie: 30 e lode!",
             userId: this.chatId,
             time:"12:30",
+            dropdownMenu: false,
           },
           {
             text:"Grande! Congratulazioni.",
             userId: 0,
             time:"12:31",
+            dropdownMenu: false,
           },
           {
             text:"Grazie :)",
             userId: this.chatId,
             time:"12:35",
+            dropdownMenu: false,
           },
         ],
         randomAnswers: [
@@ -205,9 +221,11 @@ const boolzApp = new Vue({
             text: this.textInput,
             userId: 0,
             time: date.toLocaleTimeString([],{hour: '2-digit', minute: '2-digit'}),
+            dropdownMenu: false,
         })
         setTimeout(() => {this.automaticAnswer()},3000); // Send a random answer after 3 seconds;
         this.textInput = ""; // Reset message input value
+        this.currentDate = today
       }
     },
     automaticAnswer() { // Generate a random answer
@@ -215,9 +233,26 @@ const boolzApp = new Vue({
           text: this.filterContacts[this.chatId - 1].randomAnswers[Math.floor(Math.random() * this.filterContacts[this.chatId - 1].randomAnswers.length)],
           userId: this.chatId,
           time: date.toLocaleTimeString([],{hour: '2-digit', minute: '2-digit'}),
+          dropdownMenu: false,
       })
       this.contacts[this.chatId - 1].lastAccess = date.toLocaleTimeString([],{hour: '2-digit', minute: '2-digit'});
     },
+    dropdownMenuToggle(index) { // Toggle visibility - messages dropdown menu
+      if (this.filterContacts[this.chatId - 1].chatMessages[index].dropdownMenu) {
+        this.filterContacts[this.chatId - 1].chatMessages[index].dropdownMenu = false;
+      } else {
+        this.filterContacts[this.chatId - 1].chatMessages[index].dropdownMenu = true;
+      }
+    },
+    deleteMessage(index) { // Delete a message in the chat
+      if (this.filterContacts[this.chatId - 1].chatMessages.length == 1) {
+        this.filterContacts[this.chatId - 1].chatMessages = [{}];
+        this.currentDate = null;
+      }else {
+        this.filterContacts[this.chatId - 1].chatMessages.splice(index, 1);
+
+      }
+    }
   },
   computed: { // Search chat filter
     filterContacts() {
